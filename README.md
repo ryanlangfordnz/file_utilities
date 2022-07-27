714c947028037b478b4a3c2d9e2cf5cb57e84d05
ryan <bookwyrm31415@gmail.com>
2022-07-27 20:04:53 +1200

    added renamer, minor refactoring of files

--- README.md
+++ README.md
@@ -12,5 +12,27 @@ Uses a hash of the file contents, so differently named files with identical cont
 Example, assuming you are in the directory containing deduplicator.py:
 
 ```
-$ D:\python\file_utilities\deduplicator> python deduplicator -p *path to folder I want to clean up*
+$ D:\python\file_utilities> python deduplicator -p *path to folder I want to clean up*
+```
+
+## Renamer
+
+Run from the command line with a file path as an argument, and an additional flag to specify the mode.
+
+-a: All files mode, all files at the location specified by the -p flag will be renamed to the argument given in the -a flag, keeping their file extensions.
+Example:
+
+```
+$ D:\python\file_utilities> python deduplicator -p *path to folder* -a *what to rename everything to"
+```
+
+-s: Single mode, all copies of the file specified by the -s flag will be renamed, the new name can be entered at the prompt.
+Example:
+
+```
+$ D:\python\file_utilities> python deduplicator -p *path to folder* -s *name of file you want to rename"
+There are X copies of that file
+Enter what you want to rename the files to. Copies will be in the format X_01,X_02 etc
+New name: *new name here*
+Renamed X files
 ```
--- renamer.py
+++ renamer.py
@@ -2,15 +2,33 @@ import sys
 import os
 import hashlib
 from collections import Counter
+import argparse
+import re
 
-# [0] is the filename, index 1 is the first argument
-file_path = sys.argv[1]
+# set up parser for input
+parser = argparse.ArgumentParser()
 
-# TODO
-# check sysargv fro flag
+parser.add_argument("-p", "--path", help="Path")
+parser.add_argument(
+    "-a", "--all", help="Renames all the files in the folder", required=False
+)
+parser.add_argument(
+    "-s", "--single", help="Renames all copies of specified file", required=False
+)
 
-# TODO refactor hasing into its own thing
-# if is file, rename all copies of that file to XXX_01
+args = parser.parse_args()
+
+
+if not args.path:
+    print("Please enter a path using the -p flag")
+    exit(1)
+
+if not args.single and not args.all:
+    print("Please use either the -a or -s flag")
+    exit(1)
+
+file_path = args.path
+
 # check that the path is valid and not a file
 if os.path.exists(file_path) == False or os.path.isdir(file_path) == False:
     print("Not a valid directory path sorry")
@@ -18,8 +36,27 @@ if os.path.exists(file_path) == False or os.path.isdir(file_path) == False:
 
 # get a list of everything at the path
 objects = os.listdir(file_path)
+
+
 # pare it down to just the files
-files = list(filter(lambda x: os.path.isfile(x), objects))
+files = list(filter(lambda x: os.path.isfile(file_path + "\\" + x), objects))
+
+
+# if -s flag is used, check that that file exists
+if args.single and args.single not in files:
+    print(f"File {args.single} does not exist at the specified path")
+
+# if the -a flag is used, rename all files to the specified name + _XX
+if args.all:
+    for count, value in enumerate(files):
+        extension = re.findall(r"\.\w{3}", value)[-1]
+
+        os.rename(
+            file_path + "\\" + value,
+            file_path + "\\" + args.all + f"_0{count + 1 }{extension}",
+        )
+    print(f"Renamed {len(files)} files")
+    exit(1)
 
 # Hashing the files to make sure they are unique or duplicated
 BLOCK_SIZE = 65536  # The size of each read from the file
@@ -38,5 +75,23 @@ def hasher(filename):
     return file_hash.hexdigest()
 
 
-file_hashes = {x: hasher(x) for x in files}
-hash_counter = Counter(file_hashes.values())
+# if -s flag is used and the file exists rename all copies of that file
+if args.single and args.single in files:
+    file_hashes = {x: hasher(x) for x in files}
+    hash_counter = Counter(file_hashes.values())
+    print(f"There are {hash_counter[file_hashes[args.single]]} copies of that file")
+    rename = input(
+        "Enter what you want to rename the files to. Copies will be in the format X_01,X_02 etc\nNew name:"
+    )
+    if rename == "":  # check for empty input
+        print("That is not a valid file name, please try again")
+        exit(1)
+    for count, file in enumerate(files):
+        if file_hashes[file] == file_hashes[args.single]:
+            extension = re.findall(r"\.\w{3}", file)[-1]
+            os.rename(
+                file_path + "\\" + file,
+                file_path + "\\" + rename + f"_0{count + 1 }{extension}",
+            )
+    print(f"Renamed {hash_counter[file_hashes[args.single]]} files")
+    exit(1)
